f9b616464f8ae2668c60b1ecd6d9feb63814e5b4
elawadii <elawadym44@example.com>
2024-11-05 16:50:28 +0200

    multi cc

--- routes/codeRoutes.js
+++ routes/codeRoutes.js
@@ -6,12 +6,15 @@ const makeRandom = require("../tools/makeRandom")
 const allowedTo = require("../middleware/allowedTo")
 const verifyToken = require("../middleware/verifyToken")
 const { user_roles } = require("../tools/constants/rolesConstants")
+const expressAsyncHandler = require("express-async-handler")
+const CodeModel = require("../models/CodeModel")
+const { SUCCESS } = require("../tools/statusTexts")
 
 const router = require("express").Router()
 
 router.route("/")
     .get(verifyToken(), allowedTo(user_roles.ADMIN, user_roles.SUBADMIN), getCodes)
-    .post(verifyToken(), allowedTo(user_roles.ADMIN, user_roles.SUBADMIN), (req, res, next) => {
+    .post(verifyToken(), allowedTo(user_roles.ADMIN, user_roles.SUBADMIN), expressAsyncHandler(async (req, res, next) => {
         const code = req.body
         let start
         switch (code.type) {
@@ -27,9 +30,23 @@ router.route("/")
             default:
                 return;
         }
+
+        if (code.copies > 1) {
+            if (code.copies > 500) return next(createError("اقصى عدد هو 500 كود فى العمليه الواحده", 400, FAILED))
+
+            for (let i = 0; i < code.copies; i++) {
+                const codeName = start + `${makeRandom(0, 9, 1)}-${makeRandom(0, 9, 4)}-${makeRandom(0, 9, 4)}-${makeRandom(0, 9, 4)}`
+                const createdCode = { ...code, code: codeName }
+                await CodeModel.create(createdCode)
+            }
+
+            return res.status(200).json({ message: 'تم انشاء ' + code.copies + ' اكواد', status: SUCCESS })
+        }
+
         req.body.code = start + `${makeRandom(0, 9, 1)}-${makeRandom(0, 9, 4)}-${makeRandom(0, 9, 4)}-${makeRandom(0, 9, 4)}`
         next()
-    }, createCode)
+
+    }), createCode)
 
 router.route('/user') //for user
     .get(verifyToken(), getUserUsedCodes)
