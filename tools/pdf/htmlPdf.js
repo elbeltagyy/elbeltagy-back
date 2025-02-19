4e7cca372c6a71f8a16bda2bd00bbb01398c1aee
elawadii <elawadym44@example.com>
2025-02-19 20:33:08 +0200

    phantom js

--- tools/pdf/htmlPdf.js
+++ tools/pdf/htmlPdf.js
@@ -1,8 +1,10 @@
 const pdf = require('html-pdf');
+const phantomPath = require('phantomjs').path; // Get the path to the PhantomJS binary
 
 const createPdfFromHtml = async (htmlContent, pdfPath = 'output.pdf') => {
     return new Promise((resolve, reject) => {
         const options = {
+            phantomPath: phantomPath, // Set the path to the PhantomJS binary
             format: 'A4',
             orientation: 'portrait',
             border: {
